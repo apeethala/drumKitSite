24effd39edfd6525a6fd5c69cc58cdcfc6db22b7
Alekhya Peethala <peethalaalekhya1@gmail.com>
2022-05-05 20:33:38 +0530

    drum kit

---
+++ index.js
@@ -0,0 +1,59 @@
+var buttons = document.querySelectorAll(".drum");
+for(var i=0; i< buttons.length; i++)
+{
+    buttons[i].addEventListener("click",function(){
+        
+        var buttonword = this.innerHTML;
+        makesound(buttonword);
+        buttonAnimation(buttonword);
+    });
+}
+ document.addEventListener("keydown",
+function(event)
+{ 
+    makesound(event.key);
+    buttonAnimation(event.key);
+ });
+
+ function makesound(key){
+    switch (key) {
+        case "w":
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case "a":
+                var audio = new Audio('sounds/tom-2.mp3');
+                audio.play();
+                break;
+        case "s":
+                var audio = new Audio('sounds/tom-3.mp3');
+                audio.play();
+                break;
+        case "d":
+                var audio = new Audio('sounds/tom-4.mp3');
+                audio.play();
+                break;
+        case "j":
+                var audio = new Audio('sounds/kick-bass.mp3');
+                audio.play();
+                break;
+        case "k":
+                var audio = new Audio('sounds/crash.mp3');
+                audio.play();
+                break; 
+        case "l":
+                    var audio = new Audio('sounds/snare.mp3');
+                    audio.play();
+                    break;   
+        default:
+                 console.log("invalid key");
+            break;
+    }
+
+ }
+ function buttonAnimation(key){
+    document.querySelector("."+key).classList.add("pressed");
+    setTimeout(function(){
+        document.querySelector("."+key).classList.remove("pressed"); 
+    }, 100);
+ }
